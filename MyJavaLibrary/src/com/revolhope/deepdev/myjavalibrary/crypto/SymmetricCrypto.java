1b4e1940c889a279a7e9fff493b6723fc519766a
155541 <34254684+155541@users.noreply.github.com>
2018-11-09 13:01:39 +0100

    Create SymmetricCrypto.java

---
+++ MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/SymmetricCrypto.java
@@ -0,0 +1,46 @@
+/**
+     *
+     */
+    public class SymmetricCrypto
+    {
+        private Cipher c;
+        private byte[] IV;
+        private byte[] key;
+        private String pwd;
+        
+        
+        SymmetricCrypto()
+        {
+        }
+        
+        
+        /**
+         * 
+         */
+        private SymmetricCrypto(Transformation transformation) throws NoSuchAlgorithmException, NoSuchPaddingException
+        {
+            c = Cipher.getInstance(transformation.toString());
+        }
+        
+        /**
+         * 
+         */
+        private SymmetricCipher(Transformation transformation, String provider) throws NoSuchAlgorithmException, NoSuchPaddingException, NoSuchProviderException
+        {
+            c = Cipher.getInstance(transformation.toString(), provider);
+        }
+        
+        public void setIV();
+        
+        public void setKey();
+        
+        public void setPassword();
+        
+        public void encrypt(){}
+        
+        public void decrypt(){}
+        
+        public void getIV();
+        
+        public void getKey();
+    }
