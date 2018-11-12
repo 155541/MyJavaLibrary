3e366ea8eb46753aaf3d25d0971013a74ab7c525
155541 <34254684+155541@users.noreply.github.com>
2018-11-12 17:32:16 +0100

    Update KeySizeEnum.java

--- MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/KeySizeEnum.java
+++ MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/KeySizeEnum.java
@@ -3,9 +3,13 @@ package com.revolhope.deepdev.myjavalibrary.crypto;
 public enum KeySizeEnum 
 {
 	
-	AES_128(128),
-	AES_192(192),
-	AES_256(256);
+	AES_128(128), 	// Only that i'm sure that is supported
+	AES_192(192), 	// TODO: Need to be tested
+	AES_256(256), 	// TODO: Need to be tested
+	RSA_1024(1024),	// I'm sure that is supported
+	RSA_2048(2048),	// I'm sure that is supported
+	RSA_3072(3072),	// TODO: Need to be tested
+	RSA_4096(4096); // TODO: Need to be tested
 	
 	private final int value;
 	
