f44dc6a6b5d4fdf9ab4e6f8300d6102845e29522
155541 <34254684+155541@users.noreply.github.com>
2018-11-12 18:50:41 +0100

    Update Mode.java

--- MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/Mode.java
+++ MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/Mode.java
@@ -3,5 +3,20 @@ package com.revolhope.deepdev.myjavalibrary.crypto;
 
 public enum Mode
 {
+	AES_CBC("CBC"),
+	AES_CTR("CTR");
+	// TODO: Implement more?
+	// TODO: RSA modes
 	
+	private final String mode;
+	
+	Mode(final String mode)
+	{
+		this.mode = mode;
+	}
+	
+	public String getValue()
+	{
+		return this.mode;
+	}
 }
