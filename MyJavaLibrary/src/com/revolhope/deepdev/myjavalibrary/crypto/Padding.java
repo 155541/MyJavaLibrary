abb3490e8716f4a154a095d2bd1f35d47f5ff56c
155541 <34254684+155541@users.noreply.github.com>
2018-11-12 18:54:12 +0100

    Update Padding.java

--- MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/Padding.java
+++ MyJavaLibrary/src/com/revolhope/deepdev/myjavalibrary/crypto/Padding.java
@@ -2,5 +2,21 @@ package com.revolhope.deepdev.myjavalibrary.crypto;
 
 public enum Padding 
 {
-
+    AES_NoPadding("NoPadding"),
+    AES_PKCS5Padding("PKCS7Padding"),
+    AES_PKCS7Padding("PKCS7Padding");
+    // TODO: Implement more?
+    // TODO: RSA paddings
+    
+    private final String padding;
+    
+    Padding(final String padding)
+    {
+        this.padding = padding;
+    }
+    
+    public String getValue()
+    {
+        return this.padding;
+    }
 }
